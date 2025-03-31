d8ed343d77747ae670f80d0b8364f64d7dc49221
samfip1 <samantrathod6@gmail.com>
2025-03-31 19:14:21 +0530

    Basic tamplete of dbms project

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+import mysql from "mysql2/promise";
+
+//1: connecting to the mysql server
+//2: create a db
+//3: create a table
+
+
+try {
+
+    const db = await mysql.createConnection({
+        host: "localhost",
+        user : "root",
+        password : "1234",
+        database : "dbms_wallet_system"
+    })
+    // const table = await db.execute("CREATE DATABASE IF NOT EXISTS testdb;");
+    // console.log(table);
+
+    
+    console.log(await db.execute("show databases"));
+    console.log(await db.execute("show tables;"))
+
+} catch (error) {
+    console.log(error);
+    console.log("Connected to MySql server Unsuccesssfully");
+}
+
